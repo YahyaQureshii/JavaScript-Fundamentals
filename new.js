68da23732939eac93de74afb0066e09cb171c7a3
Yahya Qureshi <120775377+YahyaQureshii@users.noreply.github.com>
2025-02-28 22:37:19 +0500

    Update new.js

--- new.js
+++ new.js
@@ -127,3 +127,118 @@ console.log(`my name is ${my_name} and my frnd is ${my_frnd}`);
 // upper & lower case convertion
 console.log(my_name.toUpperCase());
 console.log(my_name.toLowerCase());
+
+//Arrays
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr);
+console.log(arr.length)
+console.log(arr[3])
+//changing value of array with index 3 
+
+arr[3] = 22;
+console.log(arr);
+
+//Array methods
+// Push & pop
+
+let op = [1, 2, 3, 4, 5, 6];
+
+
+op.pop()
+//6 removed or poped
+op.push(100);
+
+//100 added at the end of array
+console.log(op)
+
+/* Pro Tip
+       
+    #pop Remove the last element of an array
+    #while Shift removes the first element of an array
+    
+    #push Add an element at the end of an array
+    #while unshift Add an element at the beginning of an array
+
+*/
+
+//Shift & Unshift
+
+//removed 1 from the array (start of the array)
+op.shift()
+
+//added Yahya at the start of the array
+op.unshift('yahya');
+console.log(op)
+
+// delete op[3];
+// //index 3 is deleted
+// console.log(op);
+
+//Concatination method
+
+let a1=[10,20]
+let a2=[20,30]
+let a3=[40,50]
+
+console.log(a1.concat(a2,a3)) //output [10,20,20,
+
+//Loops
+let a = [1, 93, 5, 6, 88]
+
+
+// for (let index = 0; index < a.length; index++) {
+//     const element = a[index];
+//     console.log(element)
+    
+// }
+
+// a.forEach((value, index, arr)=>{
+//     console.log(value, index, arr)
+// })
+
+// let obj = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+// }
+// for (const key in obj) {
+//     if (Object.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key]; 
+//         console.log(key, element)
+//     }
+// }
+
+for (const value of a) {
+    console.log(value)
+}
+
+
+//Maps & filters
+let arr = [1, 13, 5 ,7, 11];
+// let newArr = []
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     newArr.push(element**2)
+// }
+
+let newArr = arr.map((e, index, array)=>{
+    return e**2
+})
+
+console.log(newArr)
+const greaterThanSeven = (e)=>{
+    if(e>7){
+        return true
+    }
+    return false
+}
+console.log(arr.filter(greaterThanSeven))
+
+let arr2 = [1,2,3,4,5,6]
+
+const red = (a, b)=>{
+    return a+b
+}
+
+console.log(arr2.reduce(red))
