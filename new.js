cccce2731e8fb36b8d7eb03e96f4a0c8319a0018
Yahya Qureshi <120775377+YahyaQureshii@users.noreply.github.com>
2025-02-28 22:56:22 +0500

    Update new.js

--- new.js
+++ new.js
@@ -183,4 +183,36 @@ let a3=[40,50]
 
 console.log(a1.concat(a2,a3)) //output [10,20,20,
 
+//Loops
+let a = [1, 93, 5, 6, 88]
+
+
+// for (let index = 0; index < a.length; index++) {
+//     const element = a[index];
+//     console.log(element)
+    
+// }
+
+// a.forEach((value, index, arr)=>{
+//     console.log(value, index, arr)
+// })
+
+// let obj = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+// }
+// for (const key in obj) {
+//     if (Object.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key]; 
+//         console.log(key, element)
+//     }
+// }
+
+for (const value of a) {
+    console.log(value)
+}
+
+
+
 
